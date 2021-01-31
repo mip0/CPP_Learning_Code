18dde984d894878bb9e51dd736c0b88164ca74ed
Laefy <laefy22@gmail.com>
2021-01-31 22:49:52 +0100

    Add solutions for Chapter 4.

--- chap-04/1-hierarchy/Intern.cpp
+++ chap-04/1-hierarchy/Intern.cpp
@@ -1,19 +1,14 @@
 #include "Intern.h"
 
+#include "Rank.h"
+
 #include <random>
 #include <cstdio>
 
 Intern::Intern(std::string_view name, std::string_view surname, unsigned int salary, bool is_clumsy)
-    : _name { name }
-    , _surname { surname }
-    , _salary { salary }
+    : Employee { name, surname, salary, Rank::Slave }
     , _is_clumsy { is_clumsy }
 {}
-
-void Intern::increase_salary(unsigned int raise)
-{
-    _salary += raise;
-}
 
 void Intern::fetch_coffee()
 {
--- chap-04/3-vehicles/Vehicle.h
+++ chap-04/3-vehicles/Vehicle.h
@@ -4,23 +4,44 @@
 
 class Driver
 {
+public:
+    void pass_car_licence_exam()
+    {
+        _has_car_licence = true; 
+    }
+
+    bool has_car_licence() const
+    {
+        return _has_car_licence;
+    }
+
+    void pass_air_licence_exam()
+    {
+        _has_air_licence = true; 
+    }
+
+    bool has_air_licence() const
+    {
+        return _has_air_licence;
+    }
+
+private:
+    bool _has_car_licence = false;
+    bool _has_air_licence = false;
 };
 
 class Vehicle
 {
-public:
+protected:
     Vehicle(const Driver& driver)
         : _driver { driver }
     {}
 
+public:
     virtual ~Vehicle() {}
 
-    virtual unsigned int drive() const
-    {
-        std::cerr << "Not implemented" << std::endl;
-        return 0u;
-    }
-
-private:
+    virtual unsigned int drive() const = 0;
+    
+protected:
     const Driver& _driver;
 };
